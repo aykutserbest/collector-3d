4c7223a3dbf8de77a19d1174490024343b1d02fb
aykutserbest <aykut.serbest@yandex.com>
2022-05-13 14:27:55 +0300

    Level SO

---
+++ Assets/_GameCore/ScriptableObjects/Level.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creates a level object from the editor
+/// </summary>
+[CreateAssetMenu(fileName = "NewLevel", menuName = "ScriptableObjects/Level", order = 1)]
+public class Level : ScriptableObject
+{
+   public int levelNumber;
+   public GameObject levelMesh;
+   
+}
